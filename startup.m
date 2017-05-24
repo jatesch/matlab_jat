8841035eebbda07d031c2a7df490dd1daa94253d
Jonathan A Tesch <jtesch@jpl.nasa.gov>
2017-05-24 15:54:10 -0700

    Added startup.m

---
+++ startup.m
@@ -0,0 +1,18 @@
+% Directories
+
+addpath('~/Documents/MATLAB/export_fig');
+
+% Figure defaults
+set(0,'DefaultFigureColor','w')
+set(0,'DefaultAxesXGrid','on');
+set(0,'DefaultAxesYGrid','on');
+set(0,'DefaultAxesFontSize',12);
+
+% set(0,'defaulttextinterpreter','latex')
+
+% Set default colormap
+set(groot,'DefaultFigureColormap',jet)
+close all;
+
+% shut up
+beep off;
