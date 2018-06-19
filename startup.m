92fd63426193e6a25e11572d654eb59f0c00ee44
Jonathan Tesch <jtesch@jpl.nasa.gov>
2018-06-18 23:12:54 -0700

    Updated startup

--- startup.m
+++ startup.m
@@ -1,6 +1,22 @@
-% Directories
 
-addpath('~/Documents/MATLAB/export_fig');
+fprintf('You are jtesch!\n');
+
+wfsc_dir = '~/wfsc/';
+mljat_dir = '~/matlab_jat/';
+exfig_dir = '~/Documents/MATLAB/export_fig';
+
+
+% WFSC directory
+fprintf('Adding %s\n',wfsc_dir);
+addpath(genpath(wfsc_dir));
+
+% Add my stuff
+fprintf('Adding %s\n',mljat_dir);
+addpath(mljat_dir);
+
+% Add export_fig directory
+fprintf('Adding %s\n',exfig_dir);
+addpath(exfig_dir);
 
 % Figure defaults
 set(0,'DefaultFigureColor','w')
@@ -11,8 +27,15 @@ set(0,'DefaultAxesFontSize',12);
 % set(0,'defaulttextinterpreter','latex')
 
 % Set default colormap
-set(groot,'DefaultFigureColormap',jet)
-close all;
+%set(groot,'DefaultFigureColormap',jet)
+%close all;
 
 % shut up
 beep off;
+
+% Import wfsc/+util
+import util.*;      % import wfsc/+util functions
+
+fprintf('\n\n');
+
+
