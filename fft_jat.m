733c16e3732976ccf3ab222e4276b431af779ae5
Jonathan Tesch <jtesch@jpl.nasa.gov>
2018-07-02 21:59:34 -0700

    Added help comments

--- fft_jat.m
+++ fft_jat.m
@@ -1,6 +1,22 @@
 function [Y,om] = fft_jat(y,ts)
 % [Y,om] = fft_jat(y,ts)
 % Computes the one sided Fourier Transform F and corresponding freqency axix
+% 
+% Inputs:
+% ------
+% y : vector
+%   Time series.
+%
+% ts : scalar
+%   Sample time
+%
+% Outputs:
+% -------
+% Y : vector
+%   DFT coefficients.
+%
+% om : vector
+%   Corresponding frequency axis (Hz).
 
 M = length(y);
 w0 = 1/(M*ts);          % fundamental
